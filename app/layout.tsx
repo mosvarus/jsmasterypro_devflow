fa214bd7fa5327bbf8d742eec4bd1ea234a80b74
Varus <mosvarus@gmail.com>
2024-11-01 15:13:15 +0700

    Set up Theme, Shadcn, Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import React from "react";
+
+import Navbar from "@/components/navigation/navbar/index";
+
 import "./globals.css";
+import ThemeProvider from "../context/Theme";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -30,11 +34,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="lgiht"
+          // enableSystem
+          // disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 const Home = () => (
   <>
-    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    <h1 className="h1-bold dark:bg-slate-300 dark:text-white">
+      Welcome to the world of Next.js
+    </h1>
     <h1 className="h1-bold font-space-grotesk">
       Welcome to the world of Space Grotesk
     </h1>
