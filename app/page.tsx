067125822daa63abbac15b7ac95e35a4ed6b1f55
Varus <mosvarus@gmail.com>
2024-11-01 11:20:54 +0700

    Asset and Meatadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-const Home = () => <h1 className="h1-bold">Welcome to the world of Next.js</h1>;
-
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    <h1 className="h1-bold font-space-grotesk">
+      Welcome to the world of Space Grotesk
+    </h1>
+  </>
+);
 export default Home;
